64dad8eeb72c752f3c76af3c3af2ce82a97b747f
Alexander <rold_teem_dfc@mail.ru>
2023-01-30 19:36:55 +0300

    first commit

---
+++ Task2.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+//К сожалению, не успел
+public class Task2 {
+    static int scanNumber() {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        return number;
+
+    }
+
+    // метод получения числа операции
+    static String scanOperations() {
+        Scanner scan = new Scanner(System.in);
+        String operations = scan.nextLine();
+        return operations;
+
+    }
+
+    // метод получения результата
+    static int getResult(String opr, int num1, int num2) {
+        int result = 0;
+        switch (opr) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+
+            default:
+                System.out.println("Введите оператор: -, +, *, /");
+                break;
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Введите первое число: ");
+        int num1 = scanNumber();
+        System.out.println("Введите оператор: -, +, *, /");
+        String operations = scanOperations();
+        System.out.println("Введите второе число: ");
+        int num2 = scanNumber();
+        int result = getResult(operations, num1, num2);
+        System.out.printf("%d %s %d = %d", num1, operations, num2, result);
+
+    }
+}
